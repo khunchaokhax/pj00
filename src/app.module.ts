b170b4c0b8884f4d484bffeb676f953663ef592d
khunchaokhax <khunchaokhax@gmail.com>
2021-12-09 11:23:41 +0700

    deprod

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,39 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { CategoryModule } from './category/category.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [TodoModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'pj00',
-      autoLoadEntities: true,
-      synchronize: true,
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: '',
+    //   database: 'pj00',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: 'pj00',
+          autoLoadEntities: true,
+          synchronize: true,
+        }
+      }
+    }),
+    ConfigModule.forRoot({
+      envFilePath: ['./env/.env.' + process.env.NODE_ENV],
+      isGlobal: true,
     }),
     CategoryModule,],
   controllers: [AppController],
