275c89775eace1f8ea064054b9da29e66063502a
khunchaokhax <khunchaokhax@gmail.com>
2022-01-10 10:38:23 +0700

    create database localhost on docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,32 +9,32 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [TodoModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'pj00',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: async (configService: ConfigService) => {
-    //     return {
-    //       type: 'mysql',
-    //       host: configService.get('DB_HOST'),
-    //       port: configService.get('DB_PORT'),
-    //       username: configService.get('DB_USERNAME'),
-    //       password: configService.get('DB_PASSWORD'),
-    //       database: 'pj00',
-    //       autoLoadEntities: true,
-    //       synchronize: true,
-    //     }
-    //   }
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: '',
+    //   database: 'pj00',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
     // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: 'pj00',
+          autoLoadEntities: true,
+          synchronize: true,
+        }
+      }
+    }),
     ConfigModule.forRoot({
       envFilePath: ['./env/.env.' + process.env.NODE_ENV],
       isGlobal: true,
